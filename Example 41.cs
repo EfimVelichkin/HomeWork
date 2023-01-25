5fd1d961322028ca2cfc9656f4e6a59df0f3f017
EfimVelichkin <120294162+EfimVelichkin@users.noreply.github.com>
2023-01-25 18:46:13 +0700

    Home work for lesson 6

---
+++ Example 41.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите количество чисел ");
+int val = Convert.ToInt32(Console.ReadLine());
+int[] M = new int [val];
+int j = 0;
+for (int i = 0; i < M.Length; i++) {
+    Console.Write($"Число №{i+1} = ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    if (value > 0) {
+        j++;
+    }
+}
+Console.WriteLine($"{j} - количество положительных чисел");
---
+++ Example 43.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число b1 ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k1 ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b2 ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k2 ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+if ((k1 == k2) && (b1 == b2)) {
+    Console.WriteLine("Прямые совпадают");
+}
+else {
+    if (k1 == k2) {
+    Console.WriteLine("Прямые параллельные");
+    }
+else {
+        double x = (b2-b1)/(k1-k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"{x};{y}");
+    }
+}
